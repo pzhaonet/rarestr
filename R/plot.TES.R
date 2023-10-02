c293152fd740e11aee70c0d10c09dfbd67dca103
YiZou01 <60744976+YiZou01@users.noreply.github.com>
2023-10-02 22:44:33 +0800

    Update plot.TES.R
    Add "..." in the plot

--- R/plot.TES.R
+++ R/plot.TES.R
@@ -5,7 +5,7 @@
 #' @return a plot
 #'
 #' @examples
-plot_tes <- function(TES_output){
+plot_tes <- function(TES_output, ...){
   TESa <- TES_output$TESa
   TESb <- TES_output$TESb
   oldpar <- par(mfrow = c(1, 2),mgp=c(2.5,1,0),las=1,mar=c(4,4,2,1))
@@ -13,7 +13,14 @@ plot_tes <- function(TES_output){
     plot(1, type = "n", axes = FALSE, xlab = "", ylab = "", main="(a)")
     text(1, 1, 'NA')
   } else {
-    plot(x=TESa$result$Logm,y=TESa$result$value,xlim=c(0,2*TESa$xmax),ylim=c(0,1.2*as.numeric(TESa$par[1])), xlab = "ln(m)", ylab = "ES", main="(a)")
+    plot(x=TESa$result$Logm,
+         y=TESa$result$value,
+         xlim=c(0,2*TESa$xmax),
+         ylim=c(0,1.2*as.numeric(TESa$par[1])), 
+         xlab = "ln(m)", 
+         ylab = "ES", 
+         main="(a)", 
+        ...)
     lines(TESa$Predx, TESa$Predy, col="red")
     abline(h=TESa$par[1],lty=2)
   }
@@ -22,7 +29,14 @@ plot_tes <- function(TES_output){
     plot(1, type = "n", axes = FALSE, xlab = "", ylab = "")
     text(1, 1, 'NA')
   } else {
-    plot(x=TESb$result$Logm,y=TESb$result$value,xlim=c(0,2*TESb$xmax),ylim=c(0,1.2*as.numeric(TESb$par[1])), xlab = "ln(m)", ylab = "ES", main="(b)")
+    plot(x=TESb$result$Logm,
+         y=TESb$result$value,
+         xlim=c(0,2*TESb$xmax),
+         ylim=c(0,1.2*as.numeric(TESb$par[1])), 
+         xlab = "ln(m)", 
+         ylab = "ES", 
+         main="(b)",
+        ...)
     lines(TESb$Predx, TESb$Predy, col="red")
     abline(h=TESb$par[1],lty=2)
   }
