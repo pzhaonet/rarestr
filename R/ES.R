d09e399eee6cd585ee973371bee17f6998fe1804
Peng Zhao <pengzhao20@outlook.com>
2024-09-20 09:07:38 +0800

    remove m=1

--- R/ES.R
+++ R/ES.R
@@ -2,7 +2,7 @@
 #'
 #' @importFrom Rdpack reprompt
 #' @param x a data vector representing number of individuals for each species
-#' @param m the sample size parameter that represents the number of individuals randomly drawn from the sample, which by default is set to m=1, but can be changed according to the users' requirements. For ESa, m can not be larger than the sample size
+#' @param m the sample size parameter that represents the number of individuals randomly drawn from the sample. For ESa, m can not be larger than the sample size
 #' @param method the calculation approach of Expected Species used, with two options available as "a" and "b" to calculate ESa and ESb, with the default set as "a"
 #' @param MARGIN a vector giving the subscripts which the function will be applied over, see '\link[base]{apply}'.
 #' @return a value of Expected Species
@@ -15,7 +15,7 @@
 #' es(share, method = "b", m = 100)
 #' # When the m is larger than the total sample size, "NA" will be filled:
 #' es(share, m = 150)
-es <-  function(x, m = 1, method = c("a","b"), MARGIN = 1){
+es <-  function(x, m, method = c("a","b"), MARGIN = 1){
   method <- match.arg(method, c("a", "b"))
   if (length(dim(x)) == 2) {
     results <- apply(x, MARGIN, function(y) es(y, m, method))
